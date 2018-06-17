301148dfb1a7f4a89d3f82bc940037605bb7cf9b
daisuke <dsaito85a@gmail.com>
2018-06-18 01:27:37 +0900

    part2-1 Deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index' #pagesコントロラーのindexアクションを実行するURLを作成
-  
+  devise_for :users
+  root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
