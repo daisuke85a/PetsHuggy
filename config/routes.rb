62d228a7ad6cdb84c939953671938f494581fd7e
daisuke <dsaito85a@gmail.com>
2018-06-18 04:48:52 +0900

    3-6 プロフィールページを更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => "pages#index"
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
