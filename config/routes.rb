d6183cba044041f0eba2af1ba2ec9d71073f76fe
daisuke <dsaito85a@gmail.com>
2018-06-18 20:56:21 +0900

    4-2-1 リスティングコントローラの作成１

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :listings
   root :to => "pages#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   resources :users, only: [:show]
