ea24ee8bab00de9d6582c077dd51371da8e59228
daisuke <dsaito85a@gmail.com>
2018-06-18 21:30:17 +0900

    4-3-2 create下後に飛ぶページを変更する

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   resources :users, only: [:show]
 
+  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing_basics'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
