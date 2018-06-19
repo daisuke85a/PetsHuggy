aa67592abd74df9355e0a5c905d3a6514636c83b
daisuke <dsaito85a@gmail.com>
2018-06-19 19:36:17 +0900

    5-3 photosコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => "pages#index"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   resources :users, only: [:show]
+  resources :photos, only: [:create, :destroy]
 
   get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing_basics'
   get 'manage-listing/:id/description' => 'listings#description', as: 'manage_listing_description'
